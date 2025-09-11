0e6dffdbc1cf7a03dd1a54441534ce44d7ea3f13
Naveen Manjappa <NManjappa@manh.com>
2025-09-11 17:17:22 +0100

    ExpensesTracker Added update and delete endpoints

--- ExpensesTracker/Expenses.API/Expenses.API/Controllers/TransactionsController.cs
+++ ExpensesTracker/Expenses.API/Expenses.API/Controllers/TransactionsController.cs
@@ -62,5 +62,33 @@ namespace Expenses.API.Controllers
             return Ok(transaction);
 
         }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTransaction(int id,[FromBody]PutTransactionDto transaction)
+        {
+            var transactionDb = appDbContext.Transactions.FirstOrDefault(t => t.Id == id);
+            if (transactionDb == null)
+                return NotFound();
+            transactionDb.Type = transaction.Type;
+            transactionDb.Amount = transaction.Amount;
+            transactionDb.Category = transaction.Category;
+            transactionDb.UpdatedAt = DateTime.UtcNow;
+
+            appDbContext.Transactions.Update(transactionDb);
+            appDbContext.SaveChanges();
+            return Ok(transactionDb);
+
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public IActionResult DeleteTransaction(int id)
+        { 
+            var transactionDb = appDbContext.Transactions.FirstOrDefault(t =>t.Id == id);
+            if(transactionDb == null)
+                return NotFound();
+            appDbContext.Transactions.Remove(transactionDb);
+            appDbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
