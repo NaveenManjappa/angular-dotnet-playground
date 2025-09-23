68a66e10cd21508ec797692862d752be54e9ccc1
Naveen Manjappa <NManjappa@manh.com>
2025-09-23 13:29:22 +0100

    ExpensesTracker Designing the transactions list component

--- ExpensesTracker/Expenses.UI/src/app/components/transaction-list/transaction-list.ts
+++ ExpensesTracker/Expenses.UI/src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Transaction } from '../../models/transaction';
+import { CommonModule } from '@angular/common';
+
+'@angular/common';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './transaction-list.html',
   styleUrl: './transaction-list.css'
 })
 export class TransactionList {
-
+  transactions:Transaction[] = [
+    {
+      id: 1,
+      type: 'expense',
+      category: 'Groceries',
+      amount: 50.00,
+      createdAt: new Date('2025-09-22'),
+      updatedAt: new Date('2025-09-22')
+    },
+    {
+      id: 2,
+      type: 'income',
+      category: 'Salary',
+      amount: 2000.00,
+      createdAt: new Date('2025-09-20'),
+      updatedAt: new Date('2025-09-20')
+    }
+  ];
 }
+
