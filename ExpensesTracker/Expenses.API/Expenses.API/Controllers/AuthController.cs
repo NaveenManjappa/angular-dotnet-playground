115794c3e4168f8487a1371fb2b08ba7e7bf8b0c
Naveen Manjappa <NManjappa@manh.com>
2025-09-29 21:02:01 +0100

    ExpensesTracker Implementing the JWT based authentication

---
+++ ExpensesTracker/Expenses.API/Expenses.API/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Expenses.API.Data;
+using Expenses.API.Dtos;
+using Expenses.API.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Expenses.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+    public class AuthController(AppDbContext appDbContext) : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] PostUserDto userProfile)
+        {
+            if (appDbContext.Users.Any(u => u.Email == userProfile.Email))
+                return BadRequest("This email address is already taken");
+            
+            var newUser = new User { Email = userProfile.Email,Password=userProfile.Password,CreatedAt=DateTime.UtcNow,UpdatedAt=DateTime.UtcNow };
+
+            appDbContext.Users.Add(newUser);
+            appDbContext.SaveChanges();
+            var token = GenerateToken(newUser);
+
+            return Ok(new { Token=token});
+
+        }
+
+        private string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,user.Password)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this-is-a-very-long-lenghty-complex-hard-to-guess-and-secured-key-"));
+
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer:"expensetracker.net",
+                audience: "expensetracker.net",
+                claims:claims,
+                signingCredentials:creds,
+                expires:DateTime.UtcNow.AddHours(1)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
--- ExpensesTracker/Expenses.API/Expenses.API/Program.cs
+++ ExpensesTracker/Expenses.API/Expenses.API/Program.cs
@@ -1,19 +1,39 @@
 using Expenses.API.Data;
 using Expenses.API.Data.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(opt => opt.AddPolicy("AllowAll", opt => opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "expensetracker.net",
+        ValidAudience = "expensetracker.net",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this-is-a-very-long-lenghty-complex-hard-to-guess-and-secured-key-"))
+    };
+});
+
 var conn = builder.Configuration.GetConnectionString("Default");
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(conn));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Dependency injection
 builder.Services.AddScoped<ITransactionsService,TransactionsService>();
 
 var app = builder.Build();
@@ -26,7 +46,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
