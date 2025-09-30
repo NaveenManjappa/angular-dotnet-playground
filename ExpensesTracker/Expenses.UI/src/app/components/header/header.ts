cf61c7eab18c5322d6a3431b7b730a01b4ae0195
Naveen Manjappa <NManjappa@manh.com>
2025-09-30 21:11:47 +0100

    ExpensesTracker Implement auth guard and logout

--- ExpensesTracker/Expenses.UI/src/app/components/header/header.ts
+++ ExpensesTracker/Expenses.UI/src/app/components/header/header.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { AsyncPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
+  authService = inject(AuthService);
 }
--- ExpensesTracker/Expenses.UI/src/app/services/auth.ts
+++ ExpensesTracker/Expenses.UI/src/app/services/auth.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { AuthResponse } from '../models/authresponse';
 import { Router } from '@angular/router';
 
@@ -10,7 +10,15 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   
-  private apiUrl='https://localhost:7141/api/Auth'
+  private apiUrl='https://localhost:7141/api/Auth';
+  private currentUserSubject = new BehaviorSubject<string | null>(null);
+  currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(){
+    const token = localStorage.getItem("token");
+    if(token)
+      this.currentUserSubject.next('user');
+  }
   http:HttpClient = inject(HttpClient);
   router:Router=inject(Router);
 
@@ -19,6 +27,7 @@ export class AuthService {
                     .pipe(
                       tap(response => {
                         localStorage.setItem("token",response.token);
+                        this.currentUserSubject.next('user');
                       })
                     );
   }
@@ -28,12 +37,18 @@ export class AuthService {
                     .pipe(
                       tap(response => {
                         localStorage.setItem("token",response.token);
+                        this.currentUserSubject.next('user');
                       })
                     );
   }
 
   logout():void {
     localStorage.removeItem("token");
+    this.currentUserSubject.next(null);
     this.router.navigate(['/login']);
+  }
+
+  isAuthenticated():boolean{
+    return !!localStorage.getItem("token");
   }
 }
